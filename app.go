b0b52f51cadeb775c6d630a59ce9d55070a5bd8b
Abhishek <programwithabhi@gmail.com>
2024-09-14 19:12:06 +0530

    declaring variables with types

--- app.go
+++ app.go
@@ -6,9 +6,18 @@ import (
 )
 
 func main() {
+	/* Normal way to declare variables
 	var investmentAmount float64 = 1000
 	var expectedReturnRate float64 = 5.5
-	var years float64 = 10
+	var years float64 = 10 */
+
+	/* Shorter syntax for declaring variables
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5 */
+
+	/* Most preferred syntax for declaring variables */
+	investmentAmount := 1000.0
+	expectedReturnRate := 5.5
+	years := 10.0
 
 	var futureValue float64 = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
