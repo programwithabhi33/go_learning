24062b50c39b02151ffcd2b70d74a6bb7125a7ed
Abhishek <programwithabhi@gmail.com>
2024-09-18 11:05:08 +0530

    added the constand variable and use Scan to get user input

--- app.go
+++ app.go
@@ -15,11 +15,15 @@ func main() {
 	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5 */
 
 	/* Most preferred syntax for declaring variables */
+  const inflationRate = 2.5
 	investmentAmount := 1000.0
 	expectedReturnRate := 5.5
 	years := 10.0
 
 	var futureValue float64 = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+  futureRealValue := futureValue/math.Pow(1+inflationRate/100, years)
 
+  fmt.Scan(&investmentAmount)
 	fmt.Println("FutureValue is: ", futureValue)
+  fmt.Println("FutureRealValue is: ", futureRealValue)
 }
