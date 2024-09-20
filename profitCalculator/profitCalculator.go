9e91e85e49593eee79fa08d07e90e51e635caa6c
Abhishek <programwithabhi@gmail.com>
2024-09-20 12:15:51 +0530

    basic profitCalculator in GO

--- profitCalculator/profitCalculator.go
+++ profitCalculator/profitCalculator.go
@@ -3,5 +3,24 @@ package profitCalculator
 import "fmt"
 
 func InitializeProfitCalculator() {
-	fmt.Print("profitCalculator function")
+  var revenue float64
+  var expense float64
+  var taxRate float64
+
+  print("Enter Revenue: ")
+  fmt.Scan(&revenue)
+
+  print("Enter Expense: ")
+  fmt.Scan(&expense)
+
+  print("Enter Tax Rate: ")
+  fmt.Scan(&taxRate)
+  
+  earningBeforeTax := revenue - expense
+  profit := earningBeforeTax * (1-taxRate/100)
+  ratio := earningBeforeTax/profit
+
+  fmt.Println("Earning Before Tax is: ", earningBeforeTax)
+  fmt.Println("Profit After Tax is: ", profit)
+  fmt.Println("Ratio is: ", ratio)
 }
