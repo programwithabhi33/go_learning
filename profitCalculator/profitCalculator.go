ab27f853d4a972b591adcff74c6ba3bec94ae06d
Abhishek <programwithabhi@gmail.com>
2024-09-22 19:37:29 +0530

    Custom function in GO

--- profitCalculator/profitCalculator.go
+++ profitCalculator/profitCalculator.go
@@ -3,31 +3,37 @@ package profitCalculator
 import "fmt"
 
 func InitializeProfitCalculator() {
-  var revenue float64
-  var expense float64
-  var taxRate float64
+	var revenue float64
+	var expense float64
+	var taxRate float64
 
-  print("Enter Revenue: ")
-  fmt.Scan(&revenue)
+	outputText("Enter Revenue: ")
+	fmt.Scan(&revenue)
 
-  print("Enter Expense: ")
-  fmt.Scan(&expense)
+	outputText("Enter Expense: ")
+	fmt.Scan(&expense)
 
-  print("Enter Tax Rate: ")
-  fmt.Scan(&taxRate)
-  
-  earningBeforeTax := revenue - expense
-  profit := earningBeforeTax * (1-taxRate/100)
-  ratio := earningBeforeTax/profit
+	outputText("Enter Tax Rate: ")
+	fmt.Scan(&taxRate)
 
-  //fmt.Println("Earning Before Tax is: ", earningBeforeTax)
-  //fmt.Println("Profit After Tax is: ", profit)
-  //fmt.Println("Ratio is: ", ratio)
-  // fmt.Printf("Earnings before Tax is: %.2f\nProfit after Tax is: %.2f\nRatio is: %.2f", earningBeforeTax, profit, ratio)
-  earningBeforeTaxFV := fmt.Sprintf("Earnings before Tax is: %.2f\n", earningBeforeTax)
-  profitFV := fmt.Sprintf("Profit after Tax is: %.2f\n", profit)
-  ratioFV := fmt.Sprintf("Ratio is: %.2f", ratio)
-  print(earningBeforeTaxFV, profitFV, ratioFV)
-  print(`This is the line break statement\n
-  Second Line`)
+	earningBeforeTax := revenue - expense
+	profit := earningBeforeTax * (1 - taxRate/100)
+	ratio := earningBeforeTax / profit
+
+	//fmt.Println("Earning Before Tax is: ", earningBeforeTax)
+	//fmt.Println("Profit After Tax is: ", profit)
+	//fmt.Println("Ratio is: ", ratio)
+	// fmt.Printf("Earnings before Tax is: %.2f\nProfit after Tax is: %.2f\nRatio is: %.2f", earningBeforeTax, profit, ratio)
+	earningBeforeTaxFV := fmt.Sprintf("Earnings before Tax is: %.2f\n", earningBeforeTax)
+	profitFV := fmt.Sprintf("Profit after Tax is: %.2f\n", profit)
+	ratioFV := fmt.Sprintf("Ratio is: %.2f", ratio)
+	print(earningBeforeTaxFV, profitFV, ratioFV)
+	/*print(`This is the line break statement\n
+	  Second Line`)*/
+
+}
+
+// custom function
+func outputText(text string) {
+	print(text)
 }
