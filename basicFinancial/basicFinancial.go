c07c721a4febe0ca8902db6abeb97b2e12395384
Abhishek <programwithabhi@gmail.com>
2024-09-27 20:20:27 +0530

    basic financial application with if else in GO

--- basicFinancial/basicFinancial.go
+++ basicFinancial/basicFinancial.go
@@ -3,15 +3,34 @@ package basicFinancial
 import "fmt"
 
 func ShowChoices(){
+  accountBalance := 1000.0
   var choice int
   fmt.Println("Welcome to basic financial application!")
   fmt.Println("What do you want to do!")
   fmt.Println("1. Check balance")
-  fmt.Println("2. Withdraw money")
-  fmt.Println("3. Deposit")
+  fmt.Println("2. Deposit")
+  fmt.Println("3. Withdraw money")
   fmt.Println("4. Exit")
   
   fmt.Print("Your choice: ")
   fmt.Scan(&choice) 
-  fmt.Println("Your choice is: ", choice)
+
+  if choice == 1 {
+    fmt.Println("Your account balance is:", accountBalance)
+  }else if choice == 2 {
+    var depositAmount float64
+    fmt.Println("Your deposit:")
+    fmt.Scan(&depositAmount)
+    accountBalance += depositAmount
+    fmt.Println("Balance updated!, New account balance is:", accountBalance)
+
+  }else if choice == 3 {
+    var withdrawalAmount float64
+    fmt.Println("Withdrawal amount:")
+    fmt.Scan(&withdrawalAmount)
+    accountBalance -= withdrawalAmount 
+    fmt.Println("Withdraw successfull!, New balance is:", accountBalance)
+  }else{
+    fmt.Println("You choose to exit from application!")
+  }
 }
