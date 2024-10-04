f560fb514dca7600842b5dfd9ea2da15acd9f33a
Abhishek <programwithabhi@gmail.com>
2024-10-04 19:22:13 +0530

    switch statement added in basic financial application in GO

--- basicFinancial/basicFinancial.go
+++ basicFinancial/basicFinancial.go
@@ -16,7 +16,7 @@ func ShowChoices() {
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
 
-		if choice == 1 {
+		/*if choice == 1 {
 			fmt.Println("Your account balance is:", accountBalance)
 		} else if choice == 2 {
 			var depositAmount float64
@@ -48,7 +48,41 @@ func ShowChoices() {
 		} else {
 			fmt.Println("You choose to exit from application!")
 			break
+		}*/
+
+		switch choice {
+		case 1:
+			fmt.Println("Your account balance is:", accountBalance)
+		case 2:
+			var depositAmount float64
+			fmt.Println("Your deposit:")
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount, the deposit amount should be greater than 0")
+				continue
+			}
+			accountBalance += depositAmount
+			fmt.Println("Balance updated!, New account balance is:", accountBalance)
+		case 3:
+			var withdrawalAmount float64
+			fmt.Println("Withdrawal amount:")
+			fmt.Scan(&withdrawalAmount)
+
+			if withdrawalAmount <= 0 {
+				fmt.Println("Invalid amount, the withdrawal amount should be greater than 0")
+				continue
+			}
+			if withdrawalAmount > accountBalance {
+				fmt.Println("Invalid amount, you can't withdraw amount greater than you have in your account!")
+				continue
+			}
+			accountBalance -= withdrawalAmount
+			fmt.Println("Withdraw successfull!, New balance is:", accountBalance)
+		default:
+			fmt.Println("You choose to exit from application!")
+			fmt.Println("Thanks for using our financial application")
+			return
 		}
 	}
-	fmt.Println("Thanks for using our financial application")
 }
