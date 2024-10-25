cf3eebd133df56e84ca576e9cbce6395144f8c57
Abhishek <programwithabhi@gmail.com>
2024-10-25 07:39:24 +0530

    created a contructor function for programmer struct

--- struct/main.go
+++ struct/main.go
@@ -10,6 +10,16 @@ type programmer struct {
 	favouriteProgrammingLanguage string
 }
 
+func newProgrammer(userInputFirstName, userInputLastName, userInputAge, userInputHobby, userInputFavouriteProgrammingLanguage string) programmer {
+	return programmer{
+		firstName:                    userInputFirstName,
+		lastName:                     userInputLastName,
+		age:                          userInputAge,
+		hobby:                        userInputHobby,
+		favouriteProgrammingLanguage: userInputFavouriteProgrammingLanguage,
+	}
+}
+
 func StructMainFn() {
 	userInputFirstName := outputTextAndReturnUserInput("Enter first name")
 	userInputLastName := outputTextAndReturnUserInput("Enter last name")
@@ -18,38 +28,33 @@ func StructMainFn() {
 	userInputFavouriteProgrammingLanguage := outputTextAndReturnUserInput("Enter your favourite programming language")
 
 	var programmerData programmer
-	programmerData = programmer{
-		firstName:                    userInputFirstName,
-		lastName:                     userInputLastName,
-		age:                          userInputAge,
-		hobby:                        userInputHobby,
-		favouriteProgrammingLanguage: userInputFavouriteProgrammingLanguage,
-	}
-  programmerData.outputProgrammerDetails()
-  programmerData.clearProgrammerName()
-  programmerData.outputProgrammerDetails()
+	programmerData = newProgrammer(userInputFirstName, userInputLastName, userInputAge, userInputHobby, userInputFavouriteProgrammingLanguage)
+
+	programmerData.outputProgrammerDetails()
+	programmerData.clearProgrammerName()
+	programmerData.outputProgrammerDetails()
 }
 
 func outputTextAndReturnUserInput(outputText string) string {
 	fmt.Println(outputText)
-	var userInput string 
+	var userInput string
 	fmt.Scan(&userInput)
 	return userInput
 }
 
-func (user programmer) outputProgrammerDetails(){
-  //You access like this as well
-  //user.firstName
-  //But the technically correct to access the methods and properties on a struct pointer is
-  //(*[STRUCT_POINTER]).[FIELD_NAME] in this case e.g,. (*user).firstName
+func (user programmer) outputProgrammerDetails() {
+	//You access like this as well
+	//user.firstName
+	//But the technically correct to access the methods and properties on a struct pointer is
+	//(*[STRUCT_POINTER]).[FIELD_NAME] in this case e.g,. (*user).firstName
 
-  fmt.Println("programmer first name is:", user.firstName)
-  fmt.Println("programmer last name is:", user.lastName)
-  fmt.Println("programmer last age is:", user.age)
-  fmt.Println("programmer last hobby is:", user.hobby)
-  fmt.Println("programmer favourite language is:", user.favouriteProgrammingLanguage)
+	fmt.Println("programmer first name is:", user.firstName)
+	fmt.Println("programmer last name is:", user.lastName)
+	fmt.Println("programmer last age is:", user.age)
+	fmt.Println("programmer last hobby is:", user.hobby)
+	fmt.Println("programmer favourite language is:", user.favouriteProgrammingLanguage)
 }
-func (user *programmer) clearProgrammerName(){
-  user.firstName = ""
-  user.lastName = ""
+func (user *programmer) clearProgrammerName() {
+	user.firstName = ""
+	user.lastName = ""
 }
